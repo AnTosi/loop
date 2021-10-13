e765b1b3ef4084dcc6a0c45e03ccfdf333d12494
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-13 11:56:57 +0200

    esempio di ciclo

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/* For Loop 
+- parola chiave for
+- parentesi tonda
+    - contatore
+    - condizione
+    - incremento/decremento
+- parentesi graffe
+    - codice
+*/
+
+for (let i = 1; i < 10; i++) {
+    console.log(i*10);
+}
